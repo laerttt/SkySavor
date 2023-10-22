7e516b0f7e572943b2776111535259c61b109840
BigLali <kelvingjikola123@gmail.com>
2023-10-22 03:55:06 +0200

    token discrepancy and validation

--- static/Objects/Bundle.py
+++ static/Objects/Bundle.py
@@ -1,8 +1,11 @@
 class Bundle:
-    def __init__(self, redeemed: bool, price: int, description: str):
+    def __init__(self, redeemed: bool, price: int, description: str, header: str, picture: str, name:str):
         self._redeemed = redeemed
         self._price = price
         self._description = description
+        self._header = header
+        self._picture = picture
+        self._name=name
 
     @property
     def redeemed(self):
@@ -14,6 +17,17 @@ class Bundle:
             self._redeemed = value
         else:
             raise ValueError("Redeemed must be a boolean")
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str):
+            self._name = value
+        else:
+            raise ValueError("Name must be a boolean")
 
     @property
     def price(self):
@@ -36,5 +50,27 @@ class Bundle:
             self._description = value
         else:
             raise ValueError("Description must be a string")
+
+    @property
+    def header(self):
+        return self._header
+
+    @header.setter
+    def header(self, value):
+        if isinstance(value, str):
+            self._header = value
+        else:
+            raise ValueError("Header must be a string")
+
+    @property
+    def picture(self):
+        return self._picture
+
+    @picture.setter
+    def picture(self, value):
+        if isinstance(value, str):
+            self._picture = value
+        else:
+            raise ValueError("Picture must be a string")
 
 
